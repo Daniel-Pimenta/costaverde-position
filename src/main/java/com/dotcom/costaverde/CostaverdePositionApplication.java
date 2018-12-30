ef949d66afc0e015eea41cda73348f931797f353
Daniel-Pimenta <30393490+Daniel-Pimenta@users.noreply.github.com>
2018-12-30 18:41:01 -0200

    Ajustando querys e pagina de erro

--- src/main/java/com/dotcom/costaverde/CostaverdePositionApplication.java
+++ src/main/java/com/dotcom/costaverde/CostaverdePositionApplication.java
@@ -3,13 +3,10 @@ package com.dotcom.costaverde;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dotcom.costaverde.service.ThreadService;
-
 @SpringBootApplication
 public class CostaverdePositionApplication {
 
 	public static void main(String[] args) {
-		System.out.println("CostaverdePositionApplication.main("+args[0]+")");
 		SpringApplication.run(CostaverdePositionApplication.class, args);
 	}
 
